589ae44b7a6da9e7f6e41afcd5a25b2ee5e5a577
di_locksley <diana.iancheva@gmail.com>
2020-10-20 20:21:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int[] a = new int[n];//array for 5 elements
+            for (int i=0; i<n;i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+               // a[i] *= a[i];
+            }
+            int max=a[0];
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(max - a[i]);
+            }
+
+
+
+        }
+    }
+}
